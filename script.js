efe28d5003f4add0fead72cb8a3b52b2c5a17941
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-15 18:33:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 let x = document.querySelector('#one');
 function meta() {
-  x.innerHTML = '<h1>Entered Metaverse</h1>';
+  x.innerHTML = `<h1 id = 'status'>Entered Metaverse</h1>`;
 }
